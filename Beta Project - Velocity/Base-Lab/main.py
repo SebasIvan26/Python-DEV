9be500276804efe964061a18c6e60051a039a900
Sebastien <kseb0@hotmail.com>
2021-03-10 21:16:19 -0500

    .

--- Beta Project - Velocity/Base-Lab/main.py
+++ Beta Project - Velocity/Base-Lab/main.py
@@ -4,10 +4,6 @@
 ## PROJECT MADE WITH: Qt Designer and PyQt5
 ## V: 1.0.0
 ##
-## This project can be used freely for all uses, as long as they maintain the
-## respective credits only in the Python scripts, any information in the visual
-## interface (GUI) can be modified without any implication.
-##
 ################################################################################
 
 import sys
