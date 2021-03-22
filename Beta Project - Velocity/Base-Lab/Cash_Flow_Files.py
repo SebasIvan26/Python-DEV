86618e9d394460a997967556ce8915e768b0e768
Sebastien <kseb0@hotmail.com>
2021-03-22 15:07:02 -0400

    .

--- Beta Project - Velocity/Base-Lab/Cash_Flow_Files.py
+++ Beta Project - Velocity/Base-Lab/Cash_Flow_Files.py
@@ -55,7 +55,8 @@ def applyFormat3(ws, rowtop, rowbottom, colstart, colend):
 
     for rows in ws.iter_rows(min_row=rowtop, max_row=rowbottom, min_col=colstart, max_col=colend):
         for cell in rows:
-            cell.fill = getColorFill(boiteDeCrayon[7])    
+            cell.fill = getColorFill(boiteDeCrayon[7])
+            cell.number_format = '#,##0.00_);[Red](#,##0.00)'    
 
 def checkAndSave(wb,dest):
     wb.save(dest)
