4fcdd1bcbc4f8a62a76c3d622cb019e1cd56567a
Sebastien <kseb0@hotmail.com>
2021-04-04 19:12:04 -0400

    .

--- Beta Project - Velocity/Base-Lab/setup.py
+++ Beta Project - Velocity/Base-Lab/setup.py
@@ -1,22 +1,8 @@
-import sys
-import os
-from cx_Freeze import setup, Executable
+import PyInstaller.__main__
 
-#ADD FILES
-files = {'include_files':['icon.ico','fonts/','icons/'], 'packages':["os"]}
-
-#TARGET
-target = Executable(
-    script="main.py",
-    base="Win32GUI",
-    icon="icon.ico"
-)
-
-#SETUP CX FREEZE
-setup(  name = "GA Lab",
-        version = "0.1",
-        description = "Report automation tool",
-        author = "Sebastien St Vil",
-        options = {'build_exe': files},
-        executables = [target]
-    )
+PyInstaller.__main__.run([
+    'main.py',
+    '--onedir',
+    '--w',
+    '--icon=icon.ico'
+])
